7ced46e1e7257f288fe6187c1c64a5a440ee19d2
yejunian <leeye51456@gmail.com>
2025-05-02 16:30:37 +0900

    ✨ feat: API 라우팅에 별도 Not Found 반영

---
+++ src/app/api/route.ts
@@ -0,0 +1,23 @@
+export async function GET(request: Request) {
+  return new Response(null, { status: 404 });
+}
+
+export async function POST(request: Request) {
+  return new Response(null, { status: 404 });
+}
+
+export async function PUT(request: Request) {
+  return new Response(null, { status: 404 });
+}
+
+export async function PATCH(request: Request) {
+  return new Response(null, { status: 404 });
+}
+
+export async function DELETE(request: Request) {
+  return new Response(null, { status: 404 });
+}
+
+export async function HEAD(request: Request) {
+  return new Response(null, { status: 404 });
+}
