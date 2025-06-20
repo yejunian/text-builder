7c737a912338b70d24a03eecec2a7dd315d1d0db
yejunian <leeye51456@gmail.com>
2025-06-21 04:09:22 +0900

    🔧 config: 라이브 데이터베이스 URL 환경변수 추가

--- src/utils/server/env.ts
+++ src/utils/server/env.ts
@@ -1,4 +1,16 @@
-export const ENV_DATABASE_URL = process.env.DATABASE_URL ?? "";
+function getDatabaseUrl() {
+  if (process.env.VERCEL === "1") {
+    return process.env.LIVE_PROD_DATABASE_URL ?? "";
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    return process.env.LIVE_DEV_DATABASE_URL ?? "";
+  }
+
+  return process.env.LOCAL_DATABASE_URL ?? "";
+}
+
+export const ENV_DATABASE_URL = getDatabaseUrl();
 
 export const ENV_SECRET_ACCESS_TOKEN = new TextEncoder().encode(
   process.env.ACCESS_TOKEN_SECRET,
