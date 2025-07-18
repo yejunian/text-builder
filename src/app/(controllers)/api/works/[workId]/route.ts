8c6f5c6b4c29f0bb1b2636695986474e84c2a37d
yejunian <leeye51456@gmail.com>
2025-07-18 17:39:09 +0900

    ✨ feat: 매크로 삭제 API 구현
    추후 복원 기능을 넣었을 때의 흐름을 고려하여, 매크로 삭제 시 내부의
    필드는 그대로 유지합니다.

--- src/app/(controllers)/api/works/[workId]/route.ts
+++ src/app/(controllers)/api/works/[workId]/route.ts
@@ -3,10 +3,11 @@ import { NextRequest } from "next/server";
 import status from "http-status";
 
 import { readWork } from "@/services/works/read-work";
+import { removeWork } from "@/services/works/remove-work";
 import { userTokenUtils } from "@/utils/server/user-tokens/user-token-utils";
 
 // 계정이 소유한 작업과 작업이 포함하는 필드 조회
-export async function GET(request: NextRequest, { params }: GetContext) {
+export async function GET(request: NextRequest, { params }: RequestContext) {
   const userTokens = userTokenUtils.routeHandler(request);
 
   if (!userTokens) {
@@ -30,7 +31,28 @@ export async function GET(request: NextRequest, { params }: GetContext) {
   return Response.json(result, { status: status.OK });
 }
 
-type GetContext = {
+export async function DELETE(request: NextRequest, { params }: RequestContext) {
+  const userTokens = userTokenUtils.routeHandler(request);
+
+  if (!userTokens) {
+    return new Response(null, { status: status.UNAUTHORIZED });
+  }
+
+  const { workId } = await params;
+
+  const result = await removeWork({
+    ownerId: userTokens.access.payload.sub,
+    workId,
+  });
+
+  if (result === false) {
+    return new Response(null, { status: status.NOT_FOUND });
+  }
+
+  return Response.json(result, { status: status.OK });
+}
+
+type RequestContext = {
   params: Promise<{
     workId: string;
   }>;
