3fbeb5dc601c91f37d4d732fe4d4b75d9d359ee4
yejunian <leeye51456@gmail.com>
2025-09-04 10:39:37 +0900

    🐛 fix: 비 배포 환경 쿠키 세팅 완화

--- src/utils/server/env.ts
+++ src/utils/server/env.ts
@@ -1,14 +1,17 @@
 function getDatabaseUrl() {
-  if (process.env.VERCEL === "1") {
+  if (ENV_IS_VERCEL) {
     return process.env.LIVE_PROD_DATABASE_URL ?? "";
   }
 
-  if (process.env.NODE_ENV === "production") {
+  if (ENV_IS_PRODUCTION) {
     return process.env.LIVE_DEV_DATABASE_URL ?? "";
   }
 
   return process.env.LOCAL_DATABASE_URL ?? "";
 }
+
+export const ENV_IS_VERCEL = process.env.VERCEL === "1";
+export const ENV_IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 export const ENV_DATABASE_URL = getDatabaseUrl();
 
